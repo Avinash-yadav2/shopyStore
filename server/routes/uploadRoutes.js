d1951e080b3342821b25903e0b833ecc5563594d
Avinash Yadav <avinashyadavsonu71@gmail.com>
2026-02-14 14:57:58 +0530

    Fix uploads folder creation logic

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -1,15 +1,21 @@
 import path from 'path';
 import express from 'express';
 import multer from 'multer';
+import fs from 'fs'; 
 
 const router = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'uploads/'); 
+    const uploadPath = 'uploads/';
+
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+
+    cb(null, uploadPath);
   },
   filename(req, file, cb) {
-    
     cb(
       null,
       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
@@ -37,6 +43,7 @@ const upload = multer({
 });
 
 router.post('/', upload.single('image'), (req, res) => {
+
   res.send({
     message: 'Image Uploaded Successfully',
     image: `/${req.file.path.replace(/\\/g, '/')}`, 
